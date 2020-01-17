144b17264ddc7ad283c2146ed75768cd2f0c7d55
Samuel Gichu <itsgrapes@mb96.co>
2020-01-17 07:44:39 +0300

    Created an App Bar

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,19 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MyApp);
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
+  @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Text('Hello'),
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('QA_app'),
+        ),
+        body: Text('My App'),
+      ),
     );
   }
 }
