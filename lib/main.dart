fb565651a1e6c510ca42d467abd62293e83372be
Samuel Gichu <itsgrapes@mb96.co>
2020-01-22 08:13:23 +0300

    Using PrivateProperties inside a class

--- lib/main.dart
+++ lib/main.dart
@@ -4,15 +4,47 @@ void main() {
   runApp(MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    // TODO: implement createState
+    return _MyAppState();
+  }
+}
+
+class _MyAppState extends State<MyApp> {
+  var _questionIndex = 0;
+  void _setQuestion() {
+    setState(() {
+      _questionIndex = _questionIndex + 1;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
+    var _questions = ["question 1", "question 2", "question 3"];
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
-          title: Text('QA_app'),
+          title: Text("QA_app"),
         ),
-        body: Text('My App'),
+        body: Column(children: <Widget>[
+          Text(
+            _questions[_questionIndex],
+          ),
+          RaisedButton(
+            child: Text('Answer1'),
+            onPressed: _setQuestion,
+          ),
+          RaisedButton(
+            child: Text('Answer2'),
+            onPressed: null,
+          ),
+          RaisedButton(
+            child: Text('Answer3'),
+            onPressed: null,
+          ),
+        ]),
       ),
     );
   }
